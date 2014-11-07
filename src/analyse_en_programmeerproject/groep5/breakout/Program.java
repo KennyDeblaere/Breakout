4fc1871741c2f4c85619cec160986a2d19b17da0
Kenny Deblaere <kenny.deblaere@live.be>
2014-11-07 14:07:05 +0100

    Volledige versie gemaakt door Benjamin, Dries & Kenny

--- src/analyse_en_programmeerproject/groep5/breakout/Program.java
+++ src/analyse_en_programmeerproject/groep5/breakout/Program.java
@@ -1,12 +1,13 @@
 package analyse_en_programmeerproject.groep5.breakout;
 
-//import analyse_en_programmeerproject.groep5.breakout.view.BreakOutFrame;
-import analyse_en_programmeerproject.groep5.breakout.view.Frame;
+import analyse_en_programmeerproject.groep5.breakout.view.BreakOutFrame;
+
 
 /**
  * Created by Kenny Deblaere.
  */
 public class Program {
     public static void main(String[] args) {
+        new BreakOutFrame();
     }
 }
