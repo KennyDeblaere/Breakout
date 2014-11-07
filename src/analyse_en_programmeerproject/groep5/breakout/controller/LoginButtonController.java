75ca7d7bf462dd16999ac632589522ce56a74d01
Kenny Deblaere <kenny.deblaere@live.be>
2014-11-07 16:31:15 +0100

    Frames staan terug goed

--- src/analyse_en_programmeerproject/groep5/breakout/controller/LoginButtonController.java
+++ src/analyse_en_programmeerproject/groep5/breakout/controller/LoginButtonController.java
@@ -1,7 +1,6 @@
 package analyse_en_programmeerproject.groep5.breakout.controller;
 
 import analyse_en_programmeerproject.groep5.breakout.view.CenterPanel;
-import analyse_en_programmeerproject.groep5.breakout.view.WelcomePanel;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
