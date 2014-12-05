575d467e9db8a7558c28614749b7063102757343
Dries Dekoninck <dries.dekoninck@student.howest.be>
2014-12-05 11:03:24 +0100

    verder gewerkt aan About panel + fotootje

---
+++ src/analyse_en_programmeerproject/groep5/breakout/view/welcome/ImagePanel.java
@@ -0,0 +1,33 @@
+package analyse_en_programmeerproject.groep5.breakout.view.welcome;
+
+/**
+ * Created by Blackhat on 5/12/2014.
+ */
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+
+public class ImagePanel extends JPanel{
+
+    private BufferedImage image;
+
+    public ImagePanel() {
+        try {
+            image = ImageIO.read(new File("src\\analyse_en_programmeerproject\\groep5\\breakout\\Pictures\\Icon.jpg"));
+        } catch (IOException ex) {
+            // handle exception...
+        }
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(image, 0, 0, null); // see javadoc for more info on the parameters
+    }
+
+}
--- src/analyse_en_programmeerproject/groep5/breakout/view/welcome/PanelAbout.java
+++ src/analyse_en_programmeerproject/groep5/breakout/view/welcome/PanelAbout.java
@@ -13,6 +13,7 @@ import java.io.IOException;
 public class PanelAbout extends JPanel{
     private JPanel about;
     private JLabel titelLabel, createLabel, licensedtoLabel, versionLabel;
+    private ImagePanel imagePanel;
 
 
     public PanelAbout(){
@@ -24,11 +25,12 @@ public class PanelAbout extends JPanel{
         addComponents();
     }
     private void createComponents() {
-        about = new JPanel(new GridLayout(10,1));
+        about = new JPanel(new GridLayout(3,1));
         titelLabel = new JLabel("About");
         createLabel = new JLabel();
         licensedtoLabel = new JLabel();
         versionLabel = new JLabel();
+        imagePanel = new ImagePanel();
 
     }
 
@@ -36,12 +38,12 @@ public class PanelAbout extends JPanel{
         createLabel.setText(convertToMultiline("Created by: \n   Deblaere Kenny - CCCP | Dekoninck Dries - CCCP | Vansteelandt Benjamin - SSD"));
         licensedtoLabel.setText(convertToMultiline("Licensed to: \n   " + System.getProperty("user.name")));
         versionLabel.setText(convertToMultiline("Version: \n   1.6.4"));
-
-        about.setPreferredSize(new Dimension(500, 400));
-        about.add(titelLabel);
+        imagePanel.setPreferredSize(new Dimension(200,200));
+        about.setPreferredSize(new Dimension(500, 250));
         about.add(createLabel);
         about.add(licensedtoLabel);
         about.add(versionLabel);
+
     }
 
     private String convertToMultiline(String orig) {
@@ -49,6 +51,8 @@ public class PanelAbout extends JPanel{
     }
 
     private void addComponents() {
+        add(titelLabel);
         add(about);
+        add(imagePanel);
     }
 }
