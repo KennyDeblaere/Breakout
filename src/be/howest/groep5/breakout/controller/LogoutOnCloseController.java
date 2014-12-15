02523d5c1645a86aa18951ca595f1e279a93b967
Kenny Deblaere <kenny.deblaere@live.be>
2014-12-15 14:01:06 +0100

    Uitloggen bij afsluiten programma

---
+++ src/be/howest/groep5/breakout/controller/LogoutOnCloseController.java
@@ -0,0 +1,23 @@
+package be.howest.groep5.breakout.controller;
+
+import be.howest.groep5.breakout.data.Database;
+
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+/**
+ * Created by Kenny on 15/12/2014.
+ */
+public class LogoutOnCloseController extends WindowAdapter {
+
+    public LogoutOnCloseController() {
+
+    }
+
+    @Override
+    public void windowClosing(WindowEvent e) {
+        super.windowClosing(e);
+        Database.DatabaseInstance.updateHostNameUserLogout(Database.DatabaseInstance.getUserId());
+
+    }
+}
