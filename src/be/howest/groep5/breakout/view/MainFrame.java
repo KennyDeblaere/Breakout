5984f44999b7da29946c149a21941b1950e2e1b1
Dries <dries.dekoninck@hotmail.be>
2014-12-15 13:43:50 +0100

    Liedje

--- src/be/howest/groep5/breakout/view/MainFrame.java
+++ src/be/howest/groep5/breakout/view/MainFrame.java
@@ -1,11 +1,8 @@
 package be.howest.groep5.breakout.view;
 
+import be.howest.groep5.breakout.view.multimedia.Multimedia;
 import be.howest.groep5.breakout.view.welcome.MainPanel;
-
-import javax.imageio.ImageIO;
 import javax.swing.*;
-import java.io.File;
-import java.io.IOException;
 
 /**
  * Created by
@@ -17,19 +14,8 @@ public class MainFrame extends JFrame {
     public MainFrame() {
         super();
 
-        try {
-            setIconImage(ImageIO.read(new File("src\\be\\howest\\groep5\\breakout\\multimedia\\Icon.jpg")));
-        } catch (IOException e) {
-            System.out.println("Image laad niet in!");
-        }
-
-        try {
-
-
-        } catch(Exception ex) {
-            System.out.println("Error with playing sound.");
-            ex.printStackTrace();
-        }
+        Multimedia multimedia = new Multimedia();
+        setIconImage(multimedia.getIconImage());
 
         setTitle("Breakout (c) 2014 groep 5");
         setVisible(true);
--- src/be/howest/groep5/breakout/view/multimedia/Multimedia.java
+++ src/be/howest/groep5/breakout/view/multimedia/Multimedia.java
@@ -1,7 +1,37 @@
 package be.howest.groep5.breakout.view.multimedia;
 
+import javazoom.jl.player.Player;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.io.*;
+
 /**
  * Created by Blackhat on 15/12/2014.
  */
 public class Multimedia {
+    private BufferedImage Image;
+
+    public Multimedia() {
+        try {
+            Image = (ImageIO.read(new File("src\\be\\howest\\groep5\\breakout\\multimedia\\Icon.jpg")));
+        } catch (IOException e) {
+            System.out.println("Image laad niet in!");
+        }
+        try {
+
+            File file = new File("src/be/howest/groep5/breakout/multimedia/300.mp3");
+            FileInputStream fis = new FileInputStream(file);
+            BufferedInputStream bis = new BufferedInputStream(fis);
+            Player player = new Player(bis);
+            player.play();
+        } catch (Exception e){
+            System.out.println("Probleem bij mediaspeler!");
+        }
+    }
+
+    public BufferedImage getIconImage() {
+        return Image;
+    }
 }
