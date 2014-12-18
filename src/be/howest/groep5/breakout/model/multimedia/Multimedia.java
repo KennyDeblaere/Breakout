ee861b5e188fcdbd6c521d135df78ebd445e2523
Dries <dries.dekoninck@hotmail.be>
2014-12-18 10:38:47 +0100

    Sound aan het herstellen...

--- src/be/howest/groep5/breakout/model/multimedia/Multimedia.java
+++ src/be/howest/groep5/breakout/model/multimedia/Multimedia.java
@@ -1,23 +1,15 @@
 package be.howest.groep5.breakout.model.multimedia;
 
-import javazoom.jl.decoder.JavaLayerException;
-
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.nio.Buffer;
-import java.util.Observable;
-import java.util.Observer;
 
 /**
  * Created by Blackhat on 15/12/2014.
  */
-public class Multimedia implements Observer{
+public class Multimedia{
     private BufferedImage image, mainBackground, life;
-    private boolean playing;
-    private Sound s;
+    private boolean playing = true;
 
 
     public Multimedia() {
@@ -27,7 +19,6 @@ public class Multimedia implements Observer{
             image = (ImageIO.read(new File("src\\be\\howest\\groep5\\breakout\\multimedia\\Icon.jpg")));
             mainBackground = (ImageIO.read(new File("src\\be\\howest\\groep5\\breakout\\multimedia\\background.png")));
             life = (ImageIO.read(new File("src\\be\\howest\\groep5\\breakout\\multimedia\\heart.png")));
-            setS(new Sound());
         } catch (Exception e)/*(JavaLayerException | IOException e)*/ {
             System.out.println("Image laad niet in!");
         }
@@ -36,35 +27,14 @@ public class Multimedia implements Observer{
     public void setPlaying(boolean playing) {
         this.playing = playing;
     }
-    public void setS(Sound s) {
-        this.s = s;
-    }
 
     public boolean isPlaying() {
         return playing;
-    }
-    public Sound getS() {
-        return s;
     }
 
     public BufferedImage getIconImage() {
         return image;
     }
     public BufferedImage getMainBackground() { return  mainBackground; }
-
     public BufferedImage getLife() { return life; }
-
-
-    @Override
-    public void update(Observable o, Object arg) {
-        //TODO: Dit oplossen
-       /* if(isPlaying())
-            try {
-                getS().getPlayer().play();
-
-            } catch (JavaLayerException e) {
-                e.printStackTrace();
-            }
-*/
-    }
 }
