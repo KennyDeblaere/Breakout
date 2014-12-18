2368b14578e5bdef7a350bfcb1b772d08bfa6ace
Dries <dries.dekoninck@hotmail.be>
2014-12-18 11:42:37 +0100

    HighscorePanelCreator

--- src/be/howest/groep5/breakout/view/highscores/PanelHighScore.java
+++ src/be/howest/groep5/breakout/view/highscores/PanelHighScore.java
@@ -7,15 +7,12 @@ import java.awt.*;
  * Created by Benjamin Vansteelandt on 17/12/2014.
  */
 public class PanelHighScore extends JPanel{
-    private JLabel nameLabel, highScore;
     private JPanel singlePlayerHighscore, multiPlayerHighscore;
     private HighscorePanelCreator highscorePanelCreator;
 
 
     public PanelHighScore(){
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-        //createComponents();
-        //addSinglePlayer();
         addComponents();
         setPreferredSize(new Dimension(500, 575));
 ;
@@ -23,10 +20,7 @@ public class PanelHighScore extends JPanel{
 
     private void addComponents() {
         highscorePanelCreator = new HighscorePanelCreator();
-
-        add(new JLabel("Singleplayer"));
         add(highscorePanelCreator.createHighscorePanel(true,5));
-        add(new JLabel("Multiplayer"));
         add(highscorePanelCreator.createHighscorePanel(false,5));
     }
 }
